2bafc1e43f5a83d9bd0684abbc51586ce831767e
VikaTitaeva <vika.titaeva.2004@mail.ru>
2023-07-03 23:14:12 +0700

    кнопочки 4 этажа

--- app/src/main/java/com/example/mapnatk/FourthfloorActivity.kt
+++ app/src/main/java/com/example/mapnatk/FourthfloorActivity.kt
@@ -1,5 +1,6 @@
 package com.example.mapnatk
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -7,15 +8,65 @@ import android.view.View
 import com.davemorrissey.labs.subscaleview.ImageSource
 import com.davemorrissey.labs.subscaleview.SubsamplingScaleImageView
 import android.view.MotionEvent
+import android.widget.ImageButton
+import androidx.appcompat.app.AlertDialog
+
 class FourthfloorActivity : AppCompatActivity() {
     private val SWIPE_THRESHOLD = 100
     private var x1 = 0f
     private var x2 = 0f
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_fourthfloor)
         val imageView = findViewById<View>(R.id.imageView) as SubsamplingScaleImageView
         imageView.setImage(ImageSource.resource(R.drawable.floor4));
+
+        val button1 = findViewById<ImageButton>(R.id.imageButton1)
+        val button2 = findViewById<ImageButton>(R.id.imageButton2)
+        val button3 = findViewById<ImageButton>(R.id.imageButton3)
+        val button4 = findViewById<ImageButton>(R.id.imageButton4)
+
+        button1.setOnClickListener {            val builder = AlertDialog.Builder(this)
+            builder.setTitle(R.string.dialogTitle2)
+            builder.setMessage(R.string.dialogMessage2)
+            builder.setIcon(R.drawable.cabinet)
+            builder.setPositiveButton("Понятно"){dialogInterface, which ->
+            }
+            val alertDialog: AlertDialog = builder.create()
+            alertDialog.setCancelable(false)
+            alertDialog.show()
+        }
+        button2.setOnClickListener {            val builder = AlertDialog.Builder(this)
+            builder.setTitle(R.string.dialogTitle10)
+            builder.setMessage(R.string.dialogMessage10)
+            builder.setIcon(R.drawable.technical)
+            builder.setPositiveButton("Понятно"){dialogInterface, which ->
+            }
+            val alertDialog: AlertDialog = builder.create()
+            alertDialog.setCancelable(false)
+            alertDialog.show()
+        }
+        button3.setOnClickListener {            val builder = AlertDialog.Builder(this)
+            builder.setTitle(R.string.dialogTitle6)
+            builder.setMessage(R.string.dialogMessage6)
+            builder.setIcon(R.drawable.toilet)
+            builder.setPositiveButton("Понятно"){dialogInterface, which ->
+            }
+            val alertDialog: AlertDialog = builder.create()
+            alertDialog.setCancelable(false)
+            alertDialog.show()
+        }
+        button4.setOnClickListener {            val builder = AlertDialog.Builder(this)
+            builder.setTitle(R.string.dialogTitle4)
+            builder.setMessage(R.string.dialogMessage4)
+            builder.setIcon(R.drawable.teachmoment)
+            builder.setPositiveButton("Понятно"){dialogInterface, which ->
+            }
+            val alertDialog: AlertDialog = builder.create()
+            alertDialog.setCancelable(false)
+            alertDialog.show()
+        }
     }
     override fun onTouchEvent(event: MotionEvent): Boolean {
         when (event.action) {
