e1f768cb7c071112fab8e8e542103cef8a624fd6
VikaTitaeva <vika.titaeva.2004@mail.ru>
2023-07-03 20:55:28 +0700

    начались кнопочки

--- app/src/main/java/com/example/mapnatk/MapActivity.kt
+++ app/src/main/java/com/example/mapnatk/MapActivity.kt
@@ -3,6 +3,9 @@ package com.example.mapnatk
 import android.R.id
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.ImageButton
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.davemorrissey.labs.subscaleview.ImageSource
 import com.davemorrissey.labs.subscaleview.SubsamplingScaleImageView
@@ -14,6 +17,42 @@ class MapActivity : AppCompatActivity() {
         setContentView(R.layout.activity_map)
         val imageView = findViewById<View>(R.id.imageView) as SubsamplingScaleImageView
         imageView.setImage(ImageSource.resource(R.drawable.floor1));
+
+        val button = findViewById<ImageButton>(R.id.imageButton)
+        val button2 = findViewById<ImageButton>(R.id.imageButton3)
+        val button3 = findViewById<ImageButton>(R.id.imageButton2)
+
+        button.setOnClickListener {            val builder = AlertDialog.Builder(this)
+            builder.setTitle(R.string.dialogTitle)
+            builder.setMessage(R.string.dialogMessage)
+            builder.setIcon(android.R.drawable.ic_dialog_alert)
+            builder.setPositiveButton("Понятно"){dialogInterface, which ->
+            }
+            val alertDialog: AlertDialog = builder.create()
+            alertDialog.setCancelable(false)
+            alertDialog.show()
+        }
+        button2.setOnClickListener {            val builder = AlertDialog.Builder(this)
+            builder.setTitle(R.string.dialogTitle1)
+            builder.setMessage(R.string.dialogMessage1)
+            builder.setIcon(android.R.drawable.ic_dialog_alert)
+            builder.setPositiveButton("Понятно"){dialogInterface, which ->
+            }
+            val alertDialog: AlertDialog = builder.create()
+            alertDialog.setCancelable(false)
+            alertDialog.show()
+        }
+        button3.setOnClickListener {            val builder = AlertDialog.Builder(this)
+            builder.setTitle(R.string.dialogTitle2)
+            builder.setMessage(R.string.dialogMessage2)
+            builder.setIcon(android.R.drawable.ic_dialog_alert)
+            builder.setPositiveButton("Понятно"){dialogInterface, which ->
+            }
+            val alertDialog: AlertDialog = builder.create()
+            alertDialog.setCancelable(false)
+            alertDialog.show()
+        }
     }
 }
+
 
